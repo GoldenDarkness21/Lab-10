aeeda41c9a6330b07bf53cfadddb92cd5d42e741
Brandon Martinez <martinezbran0221@gmail.com>
2023-10-22 20:12:08 -0500

    class and prettier

--- script.js
+++ script.js
@@ -1,56 +1,69 @@
 const render = () => {
-
-// Function to fetch product data from the provided API
-const fetchProducts = async () => {
-    try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching product data:", error);
-        return [];
+  class ItemShop {
+    constructor(id, title, price, description, category, image) {
+      this.id = id;
+      this.title = title;
+      this.price = price;
+      this.description = description;
+      this.category = category;
+      this.image = image;
     }
-};
+  }
 
-// Function to render products on the page
-const renderProducts = async () => {
-    const productList = document.getElementById('product-list');
-    
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("data.json");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(":( error", error);
+      return [];
+    }
+  };
+
+  const renderProducts = async () => {
+    const productList = document.getElementById("product-list");
+
     const products = await fetchProducts();
 
-    products.forEach(product => {
-        const productItem = document.createElement('li');
-        productItem.classList.add('product-item');
-        
-        const productImage = document.createElement('img');
-        productImage.src = product.image; // Asigna la URL de la imagen del producto
-        productImage.alt = product.title; // Proporciona un texto alternativo para la imagen
+    products.forEach((productData) => {
+      const product = new ItemShop(
+        productData.id,
+        productData.title,
+        productData.price,
+        productData.description,
+        productData.category,
+        productData.image
+      );
 
-        const title = document.createElement('h2');
-        title.classList.add('product-item__title');
-        title.textContent = product.title;
-        
-        const price = document.createElement('p');
-        price.classList.add('product-item__price');
-        price.textContent = `$${product.price.toFixed(2)}`;
-        
-        const buyButton = document.createElement('button');
-        buyButton.classList.add('product-item__button');
-        buyButton.textContent = 'Comprar Ahora';
+      const productItem = document.createElement("li");
+      productItem.classList.add("product-item");
 
-        productItem.appendChild(productImage);
-        productItem.appendChild(title);
-        productItem.appendChild(price);
-        productItem.appendChild(buyButton);
+      const productImage = document.createElement("img");
+      productImage.src = product.image;
+      productImage.alt = product.title;
 
-        productList.appendChild(productItem);
+      const title = document.createElement("h2");
+      title.classList.add("product-item__title");
+      title.textContent = product.title;
+
+      const price = document.createElement("p");
+      price.classList.add("product-item__price");
+      price.textContent = `$${product.price.toFixed(2)}`;
+
+      const buyButton = document.createElement("button");
+      buyButton.classList.add("product-item__button");
+      buyButton.textContent = "Comprar Ahora";
+
+      productItem.appendChild(productImage);
+      productItem.appendChild(title);
+      productItem.appendChild(price);
+      productItem.appendChild(buyButton);
+
+      productList.appendChild(productItem);
     });
+  };
+
+  renderProducts();
 };
-
-// Call the renderProducts function to populate the product list
-renderProducts();
-
-renderProducts();
-
-}
 window.onload = render;
